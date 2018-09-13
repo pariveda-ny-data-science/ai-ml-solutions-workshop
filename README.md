db5ee342acca3ea9393233df7ebd9d797a7c1e3f
Yarbrough <jess@thewild.blue>
2018-09-13 10:02:26 -0400

    added links to other quickstarts

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ai-ml-solutions-workshop
+
+[Bing News API Python Quickstart](https://docs.microsoft.com/en-us/azure/cognitive-services/bing-news-search/python)
+https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/python
+https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/python-analyze
+https://docs.microsoft.com/en-us/azure/cognitive-services/face/QuickStarts/Python
+
+Dataset builder requires module containing subscription keys (not in source control) to function.
--- dataset_builder.py
+++ dataset_builder.py
@@ -1,19 +1,32 @@
 import subscription_keys as keys
 import requests
-from IPython.display import HTML
+import csv
 
 assert keys.azure_bing_news
 
 search_url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search"
-search_term = "Microsoft"
+search_term = input('enter search term: ')
 
 headers = {"Ocp-Apim-Subscription-Key" : keys.azure_bing_news}
-params  = {"q": search_term, "textDecorations": True, "textFormat": "HTML"}
-response = requests.get(search_url, headers=headers, params=params)
-response.raise_for_status()
-search_results = response.json()
+articles = []
 
-descriptions = [article["description"] for article in search_results["value"]]
+# retrieve 10k articles with text/image/video
+print('retrieving articles...')
+for i in range(0, 100):
+    print(str(i + 1) + ' of 100')
+    params  = {"q": search_term, 'freshness': 'Month', 'count': 100, 'offset': i * 100}
+    response = requests.get(search_url, headers = headers, params = params)
+    response.raise_for_status()
+    search_results = response.json()['value']
+    for result in search_results:
+        article = []
+        article.append(result['datePublished'])
+        article.append(result['url'])
+        article.append(result['name'])
+        article.append(result['description'])
+        articles.append(article)
 
-rows = "\n".join(["<tr><td>{0}</td></tr>".format(desc) for desc in descriptions])
-HTML("<table>"+rows+"</table>")
+with open(str(search_term) + '.csv', 'w', newline = '\n') as file:
+    writer = csv.writer(file, delimiter = ',')
+    for article in articles:
+        writer.writerow(article)
